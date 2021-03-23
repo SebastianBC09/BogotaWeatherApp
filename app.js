4e1eb3ee4a0ed333f72f198cd217f9cb0632b6d7
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-03-23 15:04:47 -0500

    Main branch reset to last commit before adding the API token.

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const https = require('https')
 
 const port = 3000
 const app = express()
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Bogotá&units=metric&"
+/* API ID deleted for security reasons */
 
 app.get("/", function(req, res) {
+  https.get(`${url}`, function(response){
+    console.log(response.statusCode)
+
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data)
+      console.log(weatherData)
+      const temp = weatherData.weather[0].description
+      console.log(temp)
+    })
+
+  })
+
   res.send("Server is up & running.")
 })
 
