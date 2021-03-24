ecdf3f5d6bc3c75f5d180cc46043b0ff3ebe3e8b
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-03-24 12:42:36 -0500

    Weather app finished. In this case the user can enter the name of the city in order to get weather info.

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
 const express = require('express')
 const https = require('https')
-
 const port = 3000
 const app = express()
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Bogotá&units=metric&"
+
 /* API ID deleted for security reasons. But get request is working great & nice. Have to check how to protect the ID token.*/
+const iconURL = "http://openweathermap.org/img/wn/"
+const size = "@2x.png"
+
+app.use(express.urlencoded({}))
 
 app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", function(req, res) {
+  const city = req.body.cityName
+  const apiKey = ""
+  const units = "metric"
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`
+
   https.get(`${url}`, function(response){
-    console.log(response.statusCode)
+  console.log(response.statusCode)
 
     response.on("data", function(data){
       const weatherData = JSON.parse(data)
-      console.log(weatherData)
-      const temp = weatherData.weather[0].description
-      console.log(temp)
+      const temp = weatherData.main.temp
+      const weatherDescription = weatherData.weather[0].description
+      const weatherIcon = weatherData.weather[0].icon
+      const getIcon = iconURL + weatherIcon + size
+      res.write(`<h1>The temperature in ${city} is ${temp} degrees celsius.</h1>`)
+      res.write(`<p>The weather is currently ${weatherDescription}.</p>`)
+      res.write(`<img src=${getIcon}></img>`)
+      res.send()
     })
-
   })
-
-  res.send("Server is up & running.")
 })
+
+
 
 
 app.listen(port, function() {
